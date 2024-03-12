ae0ea3b46a55ac4c16352ad04415c1c060d7c036
Bagjax <rifkisubagja23@gmail.com>
2024-03-12 08:37:48 +0700

    refresh jadi 5mnit

--- obj/Debug/net8.0/menyala.AssemblyInfo.cs
+++ obj/Debug/net8.0/menyala.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("menyala")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17e334ad7901f892bfc46731acc3cc3771d2328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c673b1f584a5b40089465e2bebf6bf64585f039")]
 [assembly: System.Reflection.AssemblyProductAttribute("menyala")]
 [assembly: System.Reflection.AssemblyTitleAttribute("menyala")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
